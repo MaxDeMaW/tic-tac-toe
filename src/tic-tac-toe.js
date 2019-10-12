3b0e3721bd0ae5c27ea41332cae62c92d97bc8f2
Maxim Demenkow <demenkow.maxim@gmail.com>
2019-10-12 19:39:00 +0300

    Init: start solving tic-tac-toe, solved getCurrentPlayerSymbol(), nextTurn(), getFieldValue()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,51 @@
 class TicTacToe {
     constructor() {
+        this.mySymbol = "x";
+        this.opponentSymbol = "o";
+        this.field = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
 
+        this.myTurn = true;
     }
 
     getCurrentPlayerSymbol() {
-
+        return   (this.myTurn)?  this.mySymbol: this.opponentSymbol;
     }
 
-    nextTurn(rowIndex, columnIndex) {
 
+    nextTurn(x, y) {
+        //if place isn't empty
+        if (this.field[x][y] != null) {
+        //do nothing
+        } 
+        //if place empty
+        else {
+            //set symbol depending on the player
+            this.field[x][y] = (this.myTurn == true) ? this.mySymbol : this.opponentSymbol;
+            //activate pass turn triger (next player will be not this)
+            this.myTurn = !this.myTurn;
+        }
     }
 
     isFinished() {
-
     }
 
     getWinner() {
-
     }
 
     noMoreTurns() {
-
     }
 
     isDraw() {
-
     }
 
-    getFieldValue(rowIndex, colIndex) {
-
+    getFieldValue(x, y) {
+        return this.field[x][y]
     }
 }
+
 
 module.exports = TicTacToe;
