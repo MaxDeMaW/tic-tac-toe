bc66ad97ae9d6f5b49f9c3af0660a2d5cc263b04
Maxim Demenkow <demenkow.maxim@gmail.com>
2019-10-12 20:00:45 +0300

    Feat: add solving of noMoreTurns()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -8,25 +8,31 @@ class TicTacToe {
             [null, null, null]
         ];
 
+        this.numberTurn = 1;
+
         this.myTurn = true;
     }
 
     getCurrentPlayerSymbol() {
-        return   (this.myTurn)?  this.mySymbol: this.opponentSymbol;
+        return (this.myTurn) ? this.mySymbol : this.opponentSymbol;
     }
 
 
     nextTurn(x, y) {
         //if place isn't empty
         if (this.field[x][y] != null) {
-        //do nothing
-        } 
+            //do nothing
+        }
         //if place empty
         else {
             //set symbol depending on the player
             this.field[x][y] = (this.myTurn == true) ? this.mySymbol : this.opponentSymbol;
+
             //activate pass turn triger (next player will be not this)
             this.myTurn = !this.myTurn;
+            
+            //add counter of turn number
+            this.numberTurn++;
         }
     }
 
@@ -37,12 +43,20 @@ class TicTacToe {
     }
 
     noMoreTurns() {
+        //if count of turns more than 9 - game is finishing
+        if (this.numberTurn > 9) {
+            return true
+        }
+        else {
+            return false
+        };
     }
 
     isDraw() {
     }
 
     getFieldValue(x, y) {
+        //what symbol is on this place
         return this.field[x][y]
     }
 }
