0b355289305f44c3c9ce1721c52f3a62b553b767
Maxim Demenkow <demenkow.maxim@gmail.com>
2019-10-12 20:30:31 +0300

    feat: solve getWinner() and isFinished()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,16 +1,39 @@
 class TicTacToe {
     constructor() {
+        //symbol of player 1
         this.mySymbol = "x";
+        
+        //symbol of opponent
         this.opponentSymbol = "o";
+
+        //create empty feild for game
         this.field = [
             [null, null, null],
             [null, null, null],
             [null, null, null]
         ];
 
+        //counter of turns
         this.numberTurn = 1;
 
+        //is turn 1 player?
         this.myTurn = true;
+
+        this.winCombinations = [
+            //vertical combinations
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+
+            //horizontal combinations
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+
+            //diagonal combinations
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]]
+        ]
     }
 
     getCurrentPlayerSymbol() {
@@ -30,16 +53,39 @@ class TicTacToe {
 
             //activate pass turn triger (next player will be not this)
             this.myTurn = !this.myTurn;
-            
+
             //add counter of turn number
             this.numberTurn++;
         }
     }
 
     isFinished() {
+        //if we haven't turns and have winner, game is finished
+        return  (this.noMoreTurns() || this.getWinner())? true : false;
     }
 
     getWinner() {
+        //check all combinations
+         for (var i = 0; i < this.winCombinations.length; i++) {
+
+            //for quick access get combinations
+             var placeFromTestOfWin = this.winCombinations[i];
+
+             //get winning places
+             var place1 = placeFromTestOfWin[0];
+             var place2 = placeFromTestOfWin[1];
+             var place3 = placeFromTestOfWin[2];
+
+             //check places to win
+             if (
+                this.field[place1[0]][place1[1]] == this.field[place2[0]][place2[1]] && 
+                this.field[place1[0]][place1[1]] == this.field[place3[0]][place3[1]] &&
+                this.field[place1[0]][place1[1]] !== null
+                ) {
+                 return this.field[place1[0]][place1[1]]
+             }
+         }
+         return null
     }
 
     noMoreTurns() {
